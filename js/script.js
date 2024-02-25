33891cfaccf635afa9978b07b64dec2be9b46d21
danipixel121 <156018182+Danipixel121@users.noreply.github.com>
2024-02-25 20:15:12 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.getElementById('redireccionarBtn').addEventListener('click', function() {
+    // Cambia la ruta al HTML al que deseas redirigir
+    window.location.href = 'Pokédex.html';
+  });
+  
+
+  document.getElementById('redireccionarBoton').addEventListener('click', function() {
+    // Cambia la ruta al HTML al que deseas redirigir
+    window.location.href = 'datos.html';
+  });
